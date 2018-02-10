170c127f3e4151d2f2996247839783c1e1f61544
Nander Voortman <nander@nander.net>
2018-02-10 15:39:19 +0100

    Small changes.

--- scripts/systems/render/renderSquareSprite.lua
+++ scripts/systems/render/renderSquareSprite.lua
@@ -6,13 +6,13 @@
 -- To change this template use File | Settings | File Templates.
 --
 local quads = {}
-quads.cyanTile = {4,5 }
-quads.greenTile = {4,8 }
-quads.emberTile = {4,11 }
-quads.purpleTile = {4,14 }
-quads.openDoor = {1,20 }
-quads.closedDoor= {4,20 }
+quads.cyanTile = { 4, 5 }
+quads.greenTile = { 4, 8 }
+quads.emberTile = { 4, 11 }
+quads.purpleTile = { 4, 14 }
+quads.openDoor = { 1, 20 }
+quads.closedDoor = { 0, 20 }
 
 return function(entity, args)
-    love.graphics.draw(MAPTEXTUREATLAS, scripts.systems.render.getQuad(unpack(quads[entity.tileSpriteName] or {5,0})), entity.position.x * SQUARESIZE, entity.position.y * SQUARESIZE, 0,2,2)
+    love.graphics.draw(MAPTEXTUREATLAS, scripts.systems.render.getQuad(unpack(quads[entity.tileSpriteName] or { 5, 0 })), entity.position.x * SQUARESIZE, entity.position.y * SQUARESIZE, 0, 2, 2)
 end
