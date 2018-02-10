36a4f134039ad23d9bd959b8f6a8ab5efad713ca
Nander Voortman <nander@nander.net>
2018-02-10 16:21:35 +0100

    Reformatting.

--- scripts/levels/scripts/testLevel.lua
+++ scripts/levels/scripts/testLevel.lua
@@ -8,7 +8,7 @@
 
 return function()
     LOADBYMAPFILE("assets/maps/nanders playground/test2.lua")
-    scripts.objects.door(6,8,"toggleDoor")
-    scripts.objects.movingBlock(14,14,"moveLeft", "moveRight", "moveUp", "moveDown")
-    scripts.objects.turret(6,10,2, "rotateLeft", {"toggleLaser","resetRotation1"})
+    scripts.objects.door(6, 8, "toggleDoor")
+    scripts.objects.movingBlock(14, 14, "moveLeft", "moveRight", "moveUp", "moveDown")
+    scripts.objects.turret(6, 10, 2, "rotateLeft", { "toggleLaser", "resetRotation1" })
 end
