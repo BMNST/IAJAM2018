14bed2ee15726e7ed0b9a63803908860791d1550
Nander Voortman <nander@nander.net>
2018-02-11 14:22:36 +0100

    Fix movement speed of cube.

--- scripts/actions/startActions/move.lua
+++ scripts/actions/startActions/move.lua
@@ -15,6 +15,6 @@ return function(entity, args)
     end
     local c = (entity.behavior and entity.behavior.actions) or {}
     c.move = args
-    entity.behavior = { actions = c, startTime = 0.06, time = 0.06 }
+    entity.behavior = { actions = c, startTime = 0.10, time = 0.10 }
     core.filter.update(entity)
 end
