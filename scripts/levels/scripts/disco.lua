391c8e7401c855ca060cc4ed8a47b74e15ee3e81
nander <nanderv@gmail.com>
2018-02-21 00:37:13 +0100

    Fixed game functionality.

--- scripts/levels/scripts/disco.lua
+++ scripts/levels/scripts/disco.lua
@@ -25,10 +25,5 @@ return function()
     scripts.objects.trapDoor(18, 14, false, false, nil, nil, nil, nil, nil)
     scripts.objects.trapDoor(19, 15, false, true, nil, nil, nil, nil, nil)
     scripts.objects.movingBlock(24, 11, "moveUp", "moveLeft", "moveDown", "moveRight", nil)
-    scripts.objects.movingBlock(1, 5, "moveUp", "moveLeft", "moveDown", "moveRight", nil)
-    scripts.objects.movingBlock(2, 5, "moveUp", "moveLeft", "moveDown", "moveRight", nil)
-    scripts.objects.movingBlock(1, 6, "moveUp", "moveLeft", "moveDown", "moveRight", nil)
-    scripts.objects.movingBlock(2, 6, "moveUp", "moveLeft", "moveDown", "moveRight", nil)
-
     scripts.objects.movingBlock(10, 22, "moveUp", "moveLeft", "moveDown", "moveRight", nil)
 end
--- test/test_disco.lua
+++ test/test_disco.lua
@@ -3,22 +3,19 @@ local test = {}
 
 function test.test_success()
     RUNTEST("disco", "wddswsddswswswswswswsddddadsadwwsaaaadadddsdadaaawadaddddadadsadaaaadwddsdawdaaaadadadadadadsadadadadadawdddsdawwddddd")
-    assert(TESTSTATE.finished)
-    print("SUCCESS")
+    assert_true(TESTSTATE.finished)
 end
 
 
 function test.test_too_short()
     RUNTEST("disco", "wddswsddswswswswswswsddddadsadwwsaaaadadddsdadaaawadaddddadadsadaaaadwddsdawdaaaadadadadadadsadadadadadawdddsdawwdddd")
-    assert(not TESTSTATE.finished)
-    print("SUCCESS")
+    assert_false( TESTSTATE.finished)
 end
 
 
 function test.test_death()
     RUNTEST("disco", "s")
-    assert(TESTSTATE.death)
-    print("SUCCESS")
+    assert_true(TESTSTATE.death)
 end
 
 return test
