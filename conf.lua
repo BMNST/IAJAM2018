f07ffe1e29889388cb107f5d68d84515eb098842
Simon Struck <simon@simonscode.org>
2021-10-04 22:45:46 +0200

    Update conf Love2D 0.10.2 -> 11.3

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 love.conf = function(t)
     t.title = "When the Unsinkable Happens"
-    t.version = "0.10.2"
+    t.version = "11.3"
     t.window.width = 84 * 16
     t.window.height = 48 * 16
     t.window.resizable = false
